0462c09cfbf164aa1c26606c20f68e80b5cd01a8
Fehmi Anaç <fehmianac@gmail.com>
2024-05-19 16:26:01 +0300

    test added.

---
+++ tests/Resizer.Tests/EntrypointTests.cs
@@ -0,0 +1,31 @@
+using Amazon.Lambda.APIGatewayEvents;
+using AutoFixture;
+using Xunit;
+
+namespace Resizer.Tests;
+
+public class EntrypointTests
+{
+    private readonly IFixture _fixture;
+
+    public EntrypointTests()
+    {
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public async Task ReplaceImageWithWebp()
+    {
+        Environment.SetEnvironmentVariable("BUCKET","cdn.kisshe.com");
+        Environment.SetEnvironmentVariable("PREFIX","orginal");
+        var entrypoint = new Entrypoint();
+        var response = await entrypoint.Handler(new APIGatewayProxyRequest
+        {
+            QueryStringParameters = new Dictionary<string, string>
+            {
+                { "path", "961x1200/anonymous/B39C669B-2EFF-4DDC-B04F-1FC1D011111D.webp" },
+                {"extension","jpg"}
+            }
+        }, null);
+    }
+}
